7bd8074ba3c56d0b8095a30d4c828ef4d8479d46
Victor Chung <victochung998@yahoo.com>
2019-07-16 21:33:56 -0700

    game is in workable state but needs cleanup

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,8 @@
-//4 buttons representing 4 different crystals will be displayed at the start of the Game
-//at the start of the game, a random number is assigned to the player
-//when the player clicks on the crystals, the player's total score will increase according to how much the hidden scores are and the hidden value will be revealed once the crystals are clicked
-//if the total points matches the given number, the player wins
-//if the total points went over the given number, the player lose.
-
-
-//Define global variables
+//global variables and initializing
 var currentScore = 0;
 var winCounter = 0;
 var lossCounter = 0;
-var targetScore = 0;
+var yourNumber = 0;
 var crystalBtn = $('.crystalButton');
 
 //Start Game Function
@@ -22,24 +15,24 @@ function startGame (){
     $('#wins').text(winCounter);
     $('#losses').text(lossCounter);
 
-    //Generate random target score between 19-120.
-    targetScore = Math.floor(Math.random() * (120 - 19 + 1) ) + 19;
-      console.log("target score is", targetScore);
-      $('#scoreTarget').text(targetScore);
+    //Generate random player number between 19-120.
+    yourNumber = Math.floor(Math.random() * (120 - 19 + 1) ) + 19;
+      console.log("target score is", yourNumber);
+      $('#scoreTarget').text(yourNumber);
 
     //Fill array with 4 random numbers between 1-12.
     var buttonValues = [];
     for (var i = 0; i < 4; i++) {
-        buttonValues.push(Math.round(Math.random() * 12))
+        buttonValues.push(Math.round(Math.random() * 12) +1);
         console.log(buttonValues);
     }
-        $('#button1').attr('value', buttonValues[0]);
+        $('#crystal1').attr('value', buttonValues[0]);
 
-        $('#button2').attr('value', buttonValues[1]);
+        $('#crystal2').attr('value', buttonValues[1]);
 
-        $('#button3').attr('value', buttonValues[2]);
+        $('#crystal3').attr('value', buttonValues[2]);
 
-        $('#button4').attr('value', buttonValues[3]);        
+        $('#crystal4').attr('value', buttonValues[3]);        
 }
 startGame();
 
@@ -49,32 +42,31 @@ var playGame = function(){
     currentScore = currentScore + (Number($(this).attr('value')));
     $('#scoreBox').text(currentScore);
 
-    //check if currentScore = targetScore
-    if (currentScore === targetScore){
-        $('#scoreBox').text(currentScore) //this isn't updating before alert displays
+    //comparing currentScore = player's assigned number
+    if (currentScore === yourNumber){
+        $('#scoreBox').text(currentScore) 
         winCounter++;
         $('#wins').text(winCounter);
         
-        //trigger the modal to show "you win"
+        //alert "you win"
         $('.modal').removeClass('modal');
-        $('#winloss').text('You Win!');        
+        $('#winloss').text('Congratulations!! You Win!');        
     }
-    else if(currentScore > targetScore){
+    //check to see if player's total score went over the player's assigned number
+    else if(currentScore > yourNumber){
         $('#scoreBox').text(currentScore);
         lossCounter++;
         $('#losses').text(lossCounter);
         
-        //trigger the modal to show "you lose"
+        //alert "you lose"
         $('.modal').removeClass('modal');
-        $('#winloss').text('Whoops! Your Score Exceeded the Target.');
+        $('#winloss').text('Sorry! Better luck next time.');
     }
-    else {
-        //play again
-    }
+    
 }
 
-//Set up event listener for each button
+//event listener for each button/crystal
 $('.crystalButton').on('click', playGame);
 
-//Set up event listener to restart game once the modal button is clicked 
+//event listener to restart game once the modal button is clicked 
 $('.btn-success').on('click', startGame);
